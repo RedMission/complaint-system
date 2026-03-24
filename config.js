85694c6754df94839830f45d9006f3dfbfcd940b
”RedMissiongit config --global user.email dzx0820@qq.comgit config --global user.name ”RedMission <dzx0820@qq.com>
2026-03-24 14:26:13 +0800

    配置更新

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase配置信息
+const firebaseConfig = {
+    apiKey: "AIzaSyAsu6ELDAsvyFG7aUPRvSD-yxE_uyNw-X0",
+    authDomain: "complaint-system-52993.firebaseapp.com",
+    databaseURL: "https://complaint-system-52993-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "complaint-system-52993",
+    storageBucket: "complaint-system-52993.firebasestorage.app",
+    messagingSenderId: "1025022399115",
+    appId: "1:1025022399115:web:352bf17e7e71be7ac77a8e",
+    measurementId: "G-3L6RNK4LX4"
+};
